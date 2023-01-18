ccacf4d1840472b74b652c447b311d69f95881db
Pavel_Simashchanka <simaxa7@mail.ru>
2023-01-18 11:55:49 +0200

    hw1 add input csv, package.json

--- task1.js
+++ task1.js
@@ -2,5 +2,5 @@ import { stdin, stdout } from 'node:process';
 
 stdin.on("data", data => {
     data = [...data.toString()].reverse().join('')
-    stdout.write(data + "\n")
+    stdout.write(data + "\n"+ "\n")
 })
